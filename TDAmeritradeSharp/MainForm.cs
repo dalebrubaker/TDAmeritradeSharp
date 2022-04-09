82590a0763844b9a8372b079d9ed2258353328dc
DaleBrubaker <brubaker.dale@gmail.com>
2022-04-09 10:29:09 -0400

    Add Serilog logging from GenericHost

--- TDAmeritradeSharp/MainForm.cs
+++ TDAmeritradeSharp/MainForm.cs
@@ -1,20 +1,26 @@
+using Microsoft.Extensions.Logging;
+
 namespace TDAmeritradeSharp
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private readonly ILogger<MainForm> _logger;
+
+        public MainForm(ILogger<MainForm> logger)
         {
+            _logger = logger;
             InitializeComponent();
+            _logger.LogInformation("Ctor");
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-
+            _logger.LogInformation("Loading");
         }
 
         private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-
+            _logger.LogInformation("Closing");
         }
     }
 }
--- TDAmeritradeSharp/Program.cs
+++ TDAmeritradeSharp/Program.cs
@@ -1,17 +1,54 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
 namespace TDAmeritradeSharp
 {
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainForm());
+            DefaultBuilderStart();
+        }
+
+        private static void DefaultBuilderStart()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(configuration)
+                .CreateLogger();
+            try
+            {
+                Log.Information("Application starting");
+                var host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((hostContext, services) => { services.AddScoped<MainForm>(); })
+                    .UseSerilog()
+                    .Build();
+                using var serviceScope = host.Services.CreateScope();
+                var services = serviceScope.ServiceProvider;
+                var form1 = services.GetRequiredService<MainForm>();
+                Application.Run(form1);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to run");
+                throw;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
