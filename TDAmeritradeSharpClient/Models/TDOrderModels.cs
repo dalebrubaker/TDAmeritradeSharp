2d3209c4b6775b3be6ae8a170ed106bf9a26dc0c
DaleBrubaker <brubaker.dale@gmail.com>
2022-04-12 16:40:28 -0400

    Did TDOrderModels first pass

---
+++ TDAmeritradeSharpClient/Models/TDOrderModels.cs
@@ -0,0 +1,122 @@
+﻿namespace TDAmeritradeSharpClient;
+
+// ReSharper disable InconsistentNaming
+public class CancelTime
+{
+    public string date { get; set; } = null!;
+    public bool shortFormat { get; set; }
+}
+
+public class OrderLegCollection
+{
+    public TDOrderModelsEnums.orderLegType orderLegType { get; set; }
+    public int legId { get; set; }
+    public string instrument { get; set; } = null!;
+    public TDOrderModelsEnums.instruction instruction { get; set; }
+    public TDOrderModelsEnums.positionEffect positionEffect { get; set; }
+    public int quantity { get; set; }
+    public TDOrderModelsEnums.quantityType quantityType { get; set; }
+}
+
+public class OrderInstrumentBase
+{
+    public TDOrderModelsEnums.assetType assetType { get; set; }
+    public string cusip { get; set; } = null!;
+    public string symbol { get; set; } = null!;
+    public string description { get; set; } = null!;
+}
+
+public class EquityOrderInstrument : OrderInstrumentBase
+{
+}
+
+public class FixedIncomeOrderInstrument : OrderInstrumentBase
+{
+    public string maturityDate { get; set; } = null!;
+    public double variableRate { get; set; }
+    public double factor { get; set; }
+}
+
+public class MutualFundOrderInstrument : OrderInstrumentBase
+{
+    public TDOrderModelsEnums.typeMutualFund type { get; set; }
+}
+
+public class CashEquivalentOrderInstrument : OrderInstrumentBase
+{
+    public TDOrderModelsEnums.typeCashEquivalent type { get; set; }
+}
+
+public class OptionOrderInstrument : OrderInstrumentBase
+{
+    public TDOrderModelsEnums.typeOption type { get; set; }
+    public TDOrderModelsEnums.putCall putCall { get; set; }
+    public string underlyingSymbol { get; set; } = null!;
+    public double optionMultiplier { get; set; }
+    public List<OptionDeliverables> optionDeliverables { get; set; } = null!;
+}
+
+public class OptionDeliverables
+{
+    public string symbol { get; set; } = null!;
+    public double deliverableUnits { get; set; }
+    public TDOrderModelsEnums.currencyType currencyType { get; set; }
+    public TDOrderModelsEnums.assetType assetType { get; set; }
+}
+
+public class OrderActivity
+{
+    public TDOrderModelsEnums.activityType activityType { get; set; }
+    public TDOrderModelsEnums.executionType executionType { get; set; }
+    public double quantity { get; set; }
+    public double orderRemainingQuantity { get; set; }
+    public List<ExecutionLeg> executionLegs { get; set; } = null!;
+}
+
+public class ExecutionLeg
+{
+    public double legId { get; set; }
+    public double quantity { get; set; }
+    public double mismarkedQuantity { get; set; }
+    public double price { get; set; }
+    public string time { get; set; } = null!;
+}
+
+public class Root
+{
+    public TDOrderModelsEnums.session session { get; set; }
+    public TDOrderModelsEnums.duration duration { get; set; }
+    public TDOrderModelsEnums.orderType orderType { get; set; }
+    public CancelTime cancelTime { get; set; } = null!;
+    public TDOrderModelsEnums.complexOrderStrategyType complexOrderStrategyType { get; set; }
+    public int quantity { get; set; }
+    public int filledQuantity { get; set; }
+    public int remainingQuantity { get; set; }
+    public TDOrderModelsEnums.requestedDestination requestedDestination { get; set; }
+    public string destinationLinkName { get; set; } = null!;
+    public string releaseTime { get; set; } = null!;
+    public int stopPrice { get; set; }
+    public TDOrderModelsEnums.stopPriceLinkBasis stopPriceLinkBasis { get; set; }
+    public TDOrderModelsEnums.stopPriceLinkType stopPriceLinkType { get; set; }
+    public int stopPriceOffset { get; set; }
+    public TDOrderModelsEnums.stopType stopType { get; set; }
+    public TDOrderModelsEnums.priceLinkBasis priceLinkBasis { get; set; }
+    public TDOrderModelsEnums.priceLinkType priceLinkType { get; set; }
+    public int price { get; set; }
+    public TDOrderModelsEnums.taxLotMethod taxLotMethod { get; set; }
+    public List<OrderLegCollection> orderLegCollection { get; set; } = null!;
+    public int activationPrice { get; set; }
+    public TDOrderModelsEnums.specialInstruction specialInstruction { get; set; }
+    public TDOrderModelsEnums.orderStrategyType orderStrategyType { get; set; }
+    public int orderId { get; set; }
+    public bool cancelable { get; set; }
+    public bool editable { get; set; }
+    public TDOrderModelsEnums.status status { get; set; }
+    public string enteredTime { get; set; } = null!;
+    public string closeTime { get; set; } = null!;
+    public int accountId { get; set; }
+    public List<OrderActivity> orderActivityCollection { get; set; } = null!;
+    public List<OrderActivity> replacingOrderCollection { get; set; } = null!;
+    public List<OrderActivity> childOrderStrategies { get; set; } = null!;
+    public string statusDescription { get; set; } = null!;
+}
---
+++ TDAmeritradeSharpClient/Models/TDOrderModelsEnums.cs
@@ -0,0 +1,297 @@
+﻿// ReSharper disable InconsistentNaming
+namespace TDAmeritradeSharpClient;
+
+public class TDOrderModelsEnums
+{
+    [Serializable]
+    public enum activityType
+    {
+        EXECUTION,
+        ORDER_ACTION
+    }
+
+    [Serializable]
+    public enum complexOrderStrategyType
+    {
+        NONE,
+        COVERED,
+        VERTICAL,
+        BACK_RATIO,
+        CALENDAR,
+        DIAGONAL,
+        STRADDLE,
+        STRANGLE,
+        COLLAR_SYNTHETIC,
+        BUTTERFLY,
+        CONDOR,
+        IRON_CONDOR,
+        VERTICAL_ROLL,
+        COLLAR_WITH_STOCK,
+        DOUBLE_DIAGONAL,
+        UNBALANCED_BUTTERFLY,
+        UNBALANCED_CONDOR,
+        UNBALANCED_IRON_CONDOR,
+        UNBALANCED_VERTICAL_ROLL,
+        CUSTOM
+    }
+
+    [Serializable]
+    public enum duration
+    {
+        DAY,
+        GOOD_TILL_CANCEL,
+        FILL_OR_KILL
+    }
+
+    [Serializable]
+    public enum orderType
+    {
+        MARKET,
+        LIMIT,
+        STOP,
+        STOP_LIMIT,
+        TRAILING_STOP,
+        MARKET_ON_CLOSE,
+        EXERCISE,
+        TRAILING_STOP_LIMIT,
+        NET_DEBIT,
+        NET_CREDIT,
+        NET_ZERO
+    }
+
+    [Serializable]
+    public enum requestedDestination
+    {
+        INET,
+        ECN_ARCA,
+        CBOE,
+        AMEX,
+        PHLX,
+        ISE,
+        BOX,
+        NYSE,
+        NASDAQ,
+        BATS,
+        C2,
+        AUTO
+    }
+
+    [Serializable]
+    public enum session
+    {
+        NORMAL,
+        AM,
+        PM,
+        SEAMLESS
+    }
+
+    [Serializable]
+    public enum stopPriceLinkBasis
+    {
+        MANUAL,
+        BASE,
+        TRIGGER,
+        LAST,
+        BID,
+        ASK,
+        ASK_BID,
+        MARK,
+        AVERAGE
+    }
+
+    [Serializable]
+    public enum stopPriceLinkType
+    {
+        VALUE,
+        PERCENT,
+        TICK
+    }
+
+    [Serializable]
+    public enum positionEffect
+    {
+        OPENING,
+        CLOSING,
+        AUTOMATIC
+    }
+    
+    [Serializable]
+    public enum priceLinkType
+    {
+        VALUE,
+        PERCENT,
+        TICK
+    }
+
+    [Serializable]
+    public enum stopType
+    {
+        STANDARD,
+        BID,
+        ASK,
+        LAST,
+        MARK,
+    }
+    
+    [Serializable]
+    public enum priceLinkBasis
+    {
+        MANUAL,
+        BASE,
+        TRIGGER,
+        LAST,
+        BID,
+        ASK,
+        ASK_BID,
+        MARK,
+        AVERAGE
+    }
+    
+    [Serializable]
+    public enum taxLotMethod
+    {
+        FIFO,
+        LIFO,
+        HIGH_COST,
+        LOW_COST,
+        AVERAGE_COST,
+        SPECIFIC_LOT,
+    }
+    
+    [Serializable]
+    public enum orderLegType
+    {
+        EQUITY,
+        OPTION,
+        INDEX,
+        MUTUAL_FUND,
+        CASH_EQUIVALENT,
+        FIXED_INCOME,
+        CURRENCY,
+    }
+    
+    [Serializable]
+    public enum assetType
+    {
+        EQUITY,
+        OPTION,
+        INDEX,
+        MUTUAL_FUND,
+        CASH_EQUIVALENT,
+        FIXED_INCOME,
+        CURRENCY,
+    }
+    
+    [Serializable]
+    public enum instruction
+    {
+        BUY,
+        SELL,
+        BUY_TO_COVER,
+        SELL_SHORT,
+        BUY_TO_OPEN,
+        BUY_TO_CLOSE,
+        SELL_TO_OPEN,
+        SELL_TO_CLOSE,
+        EXCHANGE
+    }
+    
+    [Serializable]
+    public enum quantityType
+    {
+        ALL_SHARES,
+        DOLLARS,
+        SHARES,
+    }
+    
+    [Serializable]
+    public enum specialInstruction
+    {
+        ALL_OR_NONE,
+        DO_NOT_REDUCE,
+        ALL_OR_NONE_DO_NOT_REDUCE,
+    }
+    
+    [Serializable]
+    public enum orderStrategyType
+    {
+        SINGLE,
+        OCO,
+        TRIGGER,
+    }
+    
+    [Serializable]
+    public enum status
+    {
+        AWAITING_PARENT_ORDER,
+        AWAITING_CONDITION,
+        AWAITING_MANUAL_REVIEW,
+        ACCEPTED,
+        AWAITING_UR_OUT,
+        PENDING_ACTIVATION,
+        QUEUED,
+        WORKING,
+        REJECTED,
+        PENDING_CANCEL,
+        CANCELED,
+        PENDING_REPLACE,
+        REPLACED,
+        FILLED,
+        EXPIRED,
+    }
+    
+    [Serializable]
+    public enum typeMutualFund
+    {
+        NOT_APPLICABLE,
+        OPEN_END_NON_TAXABLE,
+        OPEN_END_TAXABLE,
+        NO_LOAD_NON_TAXABLE,
+        NO_LOAD_TAXABLE,
+    }
+    
+    [Serializable]
+    public enum typeCashEquivalent
+    {
+        SAVINGS,
+        MONEY_MARKET_FUND,
+    }
+    
+    [Serializable]
+    public enum typeOption
+    {
+        VANILLA,
+        BINARY,
+        BARRIER,
+    }
+    
+    [Serializable]
+    public enum putCall
+    {
+        PUT,
+        CALL,
+    }
+    
+    [Serializable]
+    public enum currencyType
+    {
+        USD,
+        CAD,
+        EUR,
+        JPY,
+    }
+    
+    [Serializable]
+    public enum executionType
+    {
+        EXECUTION,
+        ORDER_ACTION,
+    }
+    
+    [Serializable]
+    public enum activityTypeExecution
+    {
+        FILL,
+    }
+
+}
